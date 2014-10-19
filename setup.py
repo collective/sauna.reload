9fda3498c157e9bfbb78c1608fef9295edbfcb72
Asko Soukka <asko.soukka@iki.fi>
2014-10-19 17:40:35 +0300

    Preparing release 0.5.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sauna.reload",
-    version='0.5.3.dev0',
+    version='0.5.3',
     description="Instant code reloading for Plone using a fork loop",
     long_description=(open("README.rst").read() + "\n" +
                       open("CHANGES.txt").read()),
