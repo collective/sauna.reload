fecd3ace8160ab26bf2ecdab92b22484fe417608
Asko Soukka <asko.soukka@iki.fi>
2019-05-08 14:12:51 +0300

    Add https://github.com/collective/sauna.reload/issues/19

--- sauna/reload/adapters.py
+++ sauna/reload/adapters.py
@@ -23,8 +23,12 @@ from zope.component import adapter
 from ZODB.interfaces import IDatabase
 from ZODB.FileStorage.FileStorage import FileStorage, read_index
 from ZEO.ClientStorage import ClientStorage
+from ZEO.cache import ClientCache
+from ZEO.asyncio.client import ClientThread
 
 from sauna.reload.interfaces import IDatabaseHooks
+
+import ZEO.asyncio.client
 
 
 @implementer(IDatabaseHooks)
@@ -98,6 +102,9 @@ class ZODBFileStorageDatabaseHooksAdapter(object):
 class ZEOClientStorageDatabaseHooksAdapter(object):
     """
     ZEO-client adapter
+
+    Plone 5.1 support contribued by csanahuja
+    https://github.com/collective/sauna.reload/issues/19
     """
 
     def __init__(self, context):
@@ -108,31 +115,53 @@ class ZEOClientStorageDatabaseHooksAdapter(object):
         self._cache_size = self.context._cache.maxsize
 
         # Close the main process' connection (before forkloop)
-        _rpc_mgr = self.context._rpc_mgr
-        _rpc_mgr.close()
+        _server = self.context._server
+        _server.close()
 
         # Close the main process' cache (before forkloop)
         self.context._cache.close()
 
     def prepareForReload(self):
-        _rpc_mgr = self.context._rpc_mgr
+        _server = self.context._server
         if self.context.is_connected():
             # Close the connection (before the child is killed)
-            _rpc_mgr.close()
+            _server.close()
 
             # Close the cache (before the child is killed)
             self.context._cache.close()
 
     def resumeFromReload(self):
         # Open a new cache for the new child
-        self.context._cache = self.context.ClientCacheClass(
+        self.context._cache = ClientCache(
             self._cache_path, size=self._cache_size)
 
         # Prepare a new connection for the new child
-        self.context._rpc_mgr = self.context.ConnectionManagerClass(
-            self.context._addr, self.context, tmin=1, tmax=30)
+        self.context._server = ClientThread(
+            self.context._addr,
+            self.context,
+            self.context._cache,
+            self.context._storage,
+            (ZEO.asyncio.client.Fallback
+             if self.context._read_only_fallback
+             else self.context._is_read_only),
+            None,
+            ssl=None,
+            ssl_server_hostname=None,
+            credentials=None
+        )
+
+        self.context._call = self.context._server.call
+        self.context._async = self.context._server.async_
+        self.context._async_iter = self.context._server.async_iter
+        self.context._wait = self.context._server.wait
 
         # Connect the new child to ZEO
-        self.context._rpc_mgr.attempt_connect()
-        if not self.context._rpc_mgr.attempt_connect():
-            self.context._rpc_mgr.connect()
+        wait = True
+        if wait:
+            try:
+                self.context._wait()
+            except Exception:
+                # No point in keeping the server going of the storage
+                # creation fails
+                self.context._server.close()
+                raise
